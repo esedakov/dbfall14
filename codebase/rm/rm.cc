dcada1dbf065e699fd0e6ddc2320bce0abeef74f
Eduard Sedakov <esedakov@msn.com>
2014-10-23 10:05:27 -0700

    constructor and several auxilary functions that are in use by ctor are implemented

--- codebase/rm/rm.cc
+++ codebase/rm/rm.cc
@@ -1,5 +1,8 @@
 
 #include "rm.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 RelationManager* RelationManager::_rm = 0;
 
@@ -11,29 +14,225 @@ RelationManager* RelationManager::instance()
     return _rm;
 }
 
-RelationManager::RelationManager()
+void RelationManager::cleanup()
 {
-	//TODO
+	remove(CATALOG_TABLE_NAME);
+	remove(CATALOG_COLUMN_NAME);
+}
+
+RC RelationManager::createRecordInTables(FileHandle tableHandle, std::vector<Attribute> table, const char* tableName, int tableId)	//NOT TESTED
+{
+	RC errCode = 0;
+
+	//determine lengths of VarChar fields
+	unsigned int lenOfTableNameField = strlen(tableName) - 1;	//-1 accounts for removing the null-character from the size of table's name
+
+	//create buffer for storing record
+	void* recordData = malloc( sizeof(unsigned int) +							//table_id
+							   (sizeof(unsigned int) + lenOfTableNameField) +	//size + table_name
+							   (sizeof(unsigned int) + lenOfTableNameField) );	//size + file_name
+
+	//copy fields into record's buffer
+	char* p = (char*) recordData;
+	//table id
+	((unsigned int*)p)[0] = tableId;
+	p += sizeof(unsigned int);
+	//table name's length
+	((unsigned int*)p)[0] = lenOfTableNameField;
+	p += sizeof(unsigned int);
+	//table name
+	memcpy(p, tableName, lenOfTableNameField);
+	p += lenOfTableNameField;
+	//file name's length
+	((unsigned int*)p)[0] = lenOfTableNameField;
+	p += sizeof(unsigned int);
+	//file name
+	memcpy(p, tableName, lenOfTableNameField);
+
+	//variable to store table's record id
+	RID tableRid;
+
+	//insert record
+	if( (errCode = _rbfm->insertRecord(tableHandle, table, recordData, tableRid)) != 0 )
+	{
+		//deallocate record's buffer
+		free(recordData);
+
+		//return error code
+		return errCode;
+	}
+
+	//deallocate record's buffer
+	free(recordData);
+
+	//return success
+	return errCode;
+}
+
+RC RelationManager::createRecordInColumns(
+		FileHandle columnHandle, std::vector<Attribute> column, int index, unsigned int tableId, const char * columnName)	//NOT TESTED
+{
+	RC errCode = 0;
+
+	//determine lengths of VarChar fields
+	unsigned int lenOfColumnNameField = strlen(columnName) - 1;	//-1 accounts for removing the null-character from the size of column's name
+
+	//create buffer for storing record
+	void* recordData = malloc( sizeof(unsigned int) +							//table_id
+							   (sizeof(unsigned int) + lenOfColumnNameField) +	//size + column_name
+							   sizeof(unsigned int) +							//column type
+							   sizeof(unsigned int));							//column length
+
+	//copy fields into record's buffer
+	char* p = (char*) recordData;
+	//table id
+	((unsigned int*)p)[0] = tableId;
+	p += sizeof(unsigned int);
+	//column name's length
+	((unsigned int*)p)[0] = lenOfColumnNameField;
+	p += sizeof(unsigned int);
+	//column name
+	memcpy(p, columnName, lenOfColumnNameField);
+	p += lenOfColumnNameField;
+	//column type
+	((unsigned int*)p)[0] = column[index].type;
+	p += sizeof(unsigned int);
+	//column length
+	((unsigned int*)p)[0] = column[index].length;
+
+	//variable to store table's record id
+	RID tableRid;
+
+	//insert record
+	if( (errCode = _rbfm->insertRecord(columnHandle, column, recordData, tableRid)) != 0 )
+	{
+		//deallocate record's buffer
+		free(recordData);
+
+		//return error code
+		return errCode;
+	}
+
+	//deallocate record's buffer
+	free(recordData);
+
+	//return success
+	return errCode;
+}
+
+void RelationManager::createCatalog()	//NOT TESTED
+{
 	//from project description:
-	//	It is mandatory to store the catalog information by using the RBF layer functions.
-	//	In other words, you should manually create the catalog's tables and populate them
-	//	the first time your database is initialized. Once the catalog's tables (such as the
-	//	Tables table with the following attributes (table-id, table-name, file-name) and
-	//	the Columns table with the following attributes (table-id, column-name,
-	//	column-type, column-length)) have been created, they should be persisted to disk.
+		//	It is mandatory to store the catalog information by using the RBF layer functions.
+		//	In other words, you should manually create the catalog's tables and populate them
+		//	the first time your database is initialized. Once the catalog's tables (such as the
+		//	Tables table with the following attributes (table-id, table-name, file-name) and
+		//	the Columns table with the following attributes (table-id, column-name,
+		//	column-type, column-length)) have been created, they should be persisted to disk.
+
+	//create new files for table and column
+	if( _rbfm->createFile(CATALOG_TABLE_NAME) != 0 || _rbfm->createFile(CATALOG_COLUMN_NAME) != 0 )
+	{
+		//abort
+		exit(-1);
+	}
+
+	//open both files
+	FileHandle tableHandle, columnHandle;
+	if( _rbfm->openFile(CATALOG_TABLE_NAME, tableHandle) != 0 || _rbfm->openFile(CATALOG_COLUMN_NAME, columnHandle) != 0 )
+	{
+		//abort
+		exit(-1);
+	}
+
+	//set both files to be modifiable solely by the DB (and not by users)
+	bool success = true;
+	tableHandle.setAccess(only_system_can_modify, success);
+	columnHandle.setAccess(only_system_can_modify, success);
+	if( success == false )
+	{
+		//abort
+		exit(-1);
+	}
+
+	//Tables table with the following attributes (table-id, table-name, file-name)
+	std::vector<Attribute> table;
+	const char* tableDescFields[] = {"table-id", "table-name", "file-name"};
+
+	table.push_back((Attribute){tableDescFields[0], TypeInt, sizeof(unsigned int)});
+	table.push_back((Attribute){tableDescFields[1], TypeVarChar, MAX_SIZE_OF_NAME_IN_DB});
+	table.push_back((Attribute){tableDescFields[2], TypeVarChar, MAX_SIZE_OF_NAME_IN_DB});
+
+	//compose and insert record describing the TABLE
+	createRecordInTables(tableHandle, table, CATALOG_TABLE_NAME, CATALOG_TABLE_ID);
+
+	//compose and insert record describing the COLUMN
+	createRecordInTables(tableHandle, table, CATALOG_COLUMN_NAME, CATALOG_COLUMN_ID);
+
+	//for fast lookup -> insert <table name, table id> INTO _catalogTable
+	_catalogTable.insert(std::pair<string, int>(CATALOG_TABLE_NAME, CATALOG_TABLE_ID));
+	_catalogTable.insert(std::pair<string, int>(CATALOG_COLUMN_NAME, CATALOG_COLUMN_ID));
+
+	//Columns table with the following attributes (table-id, column-name, column-type, column-length)
+	std::vector<Attribute> column;
+	const char* columnDescFields[] = {"table-id", "column-name", "column-type", "column-length"};
+	table.push_back((Attribute){columnDescFields[0], TypeInt, sizeof(unsigned int)});
+	table.push_back((Attribute){columnDescFields[1], TypeVarChar, MAX_SIZE_OF_NAME_IN_DB});
+	table.push_back((Attribute){columnDescFields[2], TypeInt, sizeof(unsigned int)});
+	table.push_back((Attribute){columnDescFields[3], TypeInt, sizeof(unsigned int)});
+
+	//compose and insert COLUMN records for COLUMN table
+	createRecordInColumns(columnHandle, table, 0, CATALOG_TABLE_ID, tableDescFields[0]);
+	createRecordInColumns(columnHandle, table, 1, CATALOG_TABLE_ID, tableDescFields[1]);
+	createRecordInColumns(columnHandle, table, 2, CATALOG_TABLE_ID, tableDescFields[2]);
+
+	//compose list of column information
+	std::vector<ColumnInfo> tableInfo;
+	tableInfo.push_back((ColumnInfo){tableDescFields[0], TypeInt, sizeof(unsigned int)});
+	tableInfo.push_back((ColumnInfo){tableDescFields[1], TypeVarChar, MAX_SIZE_OF_NAME_IN_DB});
+	tableInfo.push_back((ColumnInfo){tableDescFields[2], TypeVarChar, MAX_SIZE_OF_NAME_IN_DB});
+
+	//for fast lookup -> insert <table id, list of column info> for TABLE INTO _catalogColumn
+	_catatlogColumn.insert(std::pair<int, std::vector<ColumnInfo> >(CATALOG_TABLE_ID, tableInfo));
+
+	//compose and insert COLUMN records for COLUMN table
+	createRecordInColumns(columnHandle, column, 0, CATALOG_COLUMN_ID, columnDescFields[0]);
+	createRecordInColumns(columnHandle, column, 1, CATALOG_COLUMN_ID, columnDescFields[1]);
+	createRecordInColumns(columnHandle, column, 2, CATALOG_COLUMN_ID, columnDescFields[2]);
+	createRecordInColumns(columnHandle, column, 3, CATALOG_COLUMN_ID, columnDescFields[3]);
+
+	//compose list of column information
+	std::vector<ColumnInfo> columnInfo;
+	columnInfo.push_back((ColumnInfo){columnDescFields[0], TypeInt, sizeof(unsigned int)});
+	columnInfo.push_back((ColumnInfo){columnDescFields[1], TypeVarChar, MAX_SIZE_OF_NAME_IN_DB});
+	columnInfo.push_back((ColumnInfo){columnDescFields[2], TypeInt, sizeof(unsigned int)});
+	columnInfo.push_back((ColumnInfo){columnDescFields[3], TypeInt, sizeof(unsigned int)});
+
+	//for fast lookup -> insert <table id, list of column info> for TABLE INTO _catalogColumn
+	_catatlogColumn.insert(std::pair<int, std::vector<ColumnInfo> >(CATALOG_COLUMN_ID, columnInfo));
+}
+
+RelationManager::RelationManager()	//NOT TESTED
+{
+	_rbfm = RecordBasedFileManager::instance();
 
 	//check if catalog from prior execution already exists.
-	//if files for table and column did NOT existed priorly
-	//	* insert record into file for tables describing the "Tables table": {table-id:Integer, table-name:VarChar, file-name:VarChar}
-	//	* insert record into file for tables describing the "Columns table": {table-id:Integer, column-name:VarChar, column-type:???, column-length:Integer}
-	//			=> Not sure what type to assign to field "column-type" - VarChar or Integer? What is your opinion???
-	// initialize _catalogTable and insert 2 records corresponding to these two tables - Table of tables, Table of columns
-	//-------------------
-	//	* insert records into file for columns describing each of the columns of two aforementioned tables (e.g. table-id, column-type, ...)
-	// initialize _catatlogColumn and insert 7 records corresponding to 7 different columns from the 2 aforementioned tables
+	FileHandle catalogOfTables, catalogOfColumns;
 
-	//else (these files existed)
-	//	* May be it is good idea to make sure that each such record stores correct information (optional???)
+	std::string catalogTableFileName = string(CATALOG_TABLE_NAME), catalogColumnFileName = string(CATALOG_COLUMN_NAME);
+
+	if( _rbfm->openFile(catalogTableFileName, catalogOfTables) != 0 || _rbfm->openFile(catalogColumnFileName, catalogOfColumns) != 0 )
+	{
+		//if some of the system files do not exist, clean up
+		cleanup();
+
+		//and compile new catalog files
+		createCatalog();
+	}
+	else
+	{
+		//TODO: check that content of the catalog is consistent
+	}
 }
 
 RelationManager::~RelationManager()
